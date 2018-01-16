e23f46fee991c17baed3c3f63760e461391f71f0
Jan Rembold <jan.rembold@virtual-identity.com>
2018-01-16 11:57:36 +0100

    removed surrounding function in image pipe

--- pipes/image.js
+++ pipes/image.js
@@ -2,20 +2,18 @@ const gulp = require('gulp');
 
 module.exports = {
 	minifyAll: (source, target, config, cb) => {
-		return () => {
-			const pump = require('pump');
-			const image = require('gulp-imagemin');
+		const pump = require('pump');
+		const image = require('gulp-imagemin');
 
-			pump([
-				gulp.src(source),
-				image([
-					image.gifsicle(),
-					image.jpegtran(),
-					image.optipng(),
-					image.svgo()
-				], config.image),
-				gulp.dest(target)
-			], cb);
-		};
+		pump([
+			gulp.src(source),
+			image([
+				image.gifsicle(),
+				image.jpegtran(),
+				image.optipng(),
+				image.svgo()
+			], config.image),
+			gulp.dest(target)
+		], cb);
 	}
 };
